ea35f57513a2625e141af9365c143d3a01158ee3
JackOMan69 <38285219+JackOman69@users.noreply.github.com>
2018-09-10 16:30:41 +0400

    upd

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ def handle_start(message):
 		bot.send_message(message.chat.id, "Добро пожаловать...", + 
 			"\n" + "Первое объявление: " + 
 			"\n" + "Название: " + res[0]["title"] + '\n' + "Описание: " + res[0]["req"] + 
-    	"\n" + "Цена: " + str(res[0]["price"]) + "\n" + "Категория: " + str(res[0]["category"]) + 
+    	"\n" + "Цена: " + res[0]["price"] + "\n" + "Категория: " + res[0]["category"] + 
     	"\n" + "Кол-во заявок на выполнение: " + res[0]["rates_num"], reply_markup=keyboard)
 
 
