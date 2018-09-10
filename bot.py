1832a29c89df904df193f0d46803a73dc9d7f3f5
JackOMan69 <38285219+JackOman69@users.noreply.github.com>
2018-09-10 16:44:19 +0400

    upd

--- bot.py
+++ bot.py
@@ -1,19 +1,35 @@
-﻿import telebot
+import telebot
 from config import token
 from freelance_response import Response
 
 bot = telebot.TeleBot(token)
 
+name = "Программист"
+object = Response(name)
+res = object.get_dict()
+
 @bot.message_handler(commands=["start"])
 def handle_start(message):
-		name = "Программист"
-		object = Response(name)
-		res = object.get_dict()
-		bot.send_message(message.chat.id, "Добро пожаловать...", + 
-			"\n" + "Первое объявление: " + 
-			"\n" + "Название: " + res[0]["title"] + '\n' + "Описание: " + res[0]["req"] + 
-    	"\n" + "Цена: " + res[0]["price"] + "\n" + "Категория: " + res[0]["category"] + 
-    	"\n" + "Кол-во заявок на выполнение: " + str(res[0]["rates_num"]), reply_markup=keyboard)
+		global name
+		global res
+		bot.send_message(message.chat.id, "Первое объявление: " +
+			"\n" + "Название: " + res[0]["title"] + '\n' + "Описание: " + res[0]["req"] +
+    	"\n" + "Цена: " + res[0]["price"] + "\n" + "Категория: " + res[0]["category"] +
+    	"\n" + "Кол-во заявок на выполнение: " + res[0]["rates_num"], reply_markup=keyboard)
 
 
 bot.polling(none_stop=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
