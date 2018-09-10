a262353d5f447f2e9830c20f6a29d67b389b5700
JackOMan69 <38285219+JackOman69@users.noreply.github.com>
2018-09-10 16:57:39 +0400

    upd

--- bot.py
+++ bot.py
@@ -4,14 +4,11 @@ from freelance_response import Response
 
 bot = telebot.TeleBot(token)
 
-name = "Программист"
-object = Response(name)
-res = object.get_dict()
-
 @bot.message_handler(commands=["start"])
 def handle_start(message):
-		global name
-		global res
+		name = "Программист"
+		object = Response(name)
+		res = object.get_dict()
 		bot.send_message(message.chat.id, "Первое объявление: " +
 			"\n" + "Название: " + res[0]["title"] + '\n' + "Описание: " + res[0]["req"] +
     	"\n" + "Цена: " + str(res[0]["price"]) + "\n" + "Категория: " + str(res[0]["category"]) +
